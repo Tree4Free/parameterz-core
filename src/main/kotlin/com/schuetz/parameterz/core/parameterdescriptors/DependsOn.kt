3437e83fce30d85db770f5bfdfd77825ab2d7e75
Roland Schütz <schuetzroland@outlook.com>
2020-09-20 20:21:55 +0200

    Add upload to mvn central

--- src/main/kotlin/com/schuetz/parameterz/core/parameterdescriptors/DependsOn.kt
+++ src/main/kotlin/com/schuetz/parameterz/core/parameterdescriptors/DependsOn.kt
@@ -1,27 +1,27 @@
 package com.schuetz.parameterz.core.parameterdescriptors
 
-abstract class DependsOn(val type: Type) {
+abstract class DependsOn(val type: _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn.Type) {
 
-    class AndDependsOn(val dependencies: Collection<DependsOn>) : DependsOn(Type.AND)
-    class OrDependsOn(val dependencies: Collection<DependsOn>) : DependsOn(Type.OR)
-    class NotDependsOn(val parameter: String, val value: String) : DependsOn(Type.NOT)
-    class JustDependsOn(val parameter: String, val value: String) : DependsOn(Type.JUST)
+    class AndDependsOn(val dependencies: Collection<_root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn>) : _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn(_root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn.Type.AND)
+    class OrDependsOn(val dependencies: Collection<_root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn>) : _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn(_root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn.Type.OR)
+    class NotDependsOn(val parameter: String, val value: String) : _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn(_root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn.Type.NOT)
+    class JustDependsOn(val parameter: String, val value: String) : _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn(_root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn.Type.JUST)
 
     companion object {
-        fun not(parameter: String, value: String): DependsOn {
-            return NotDependsOn(parameter, value)
+        fun not(parameter: String, value: String): _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn {
+            return _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn.NotDependsOn(parameter, value)
         }
 
-        fun just(parameter: String, value: String): DependsOn {
-            return JustDependsOn(parameter, value)
+        fun just(parameter: String, value: String): _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn {
+            return _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn.JustDependsOn(parameter, value)
         }
 
-        fun and(vararg deps: DependsOn): DependsOn {
-            return AndDependsOn(deps.toList())
+        fun and(vararg deps: _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn): _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn {
+            return _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn.AndDependsOn(deps.toList())
         }
 
-        fun or(vararg deps: DependsOn): DependsOn {
-            return OrDependsOn(deps.toList())
+        fun or(vararg deps: _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn): _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn {
+            return _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn.OrDependsOn(deps.toList())
         }
     }
 
--- src/main/kotlin/com/schuetz/parameterz/core/parameterdescriptors/translation/TranslatedParameter.kt
+++ src/main/kotlin/com/schuetz/parameterz/core/parameterdescriptors/translation/TranslatedParameter.kt
@@ -1,6 +1,5 @@
 package com.schuetz.parameterz.core.parameterdescriptors.translation
 
-import com.schuetz.parameterz.core.parameterdescriptors.DependsOn
 import com.schuetz.parameterz.core.parameterdescriptors.Parameter
 import com.schuetz.parameterz.core.parameterdescriptors.type.ParameterType
 import com.schuetz.parameterz.core.parameterdescriptors.validation.Validation
@@ -14,7 +13,7 @@ class TranslatedParameter(
     type: ParameterType,
     validation: Collection<Validation>,
     defaultValue: String,
-    dependsOn: DependsOn?,
+    dependsOn: _root_ide_package_.com.schuetz.parameterz.core.parameterdescriptors.DependsOn?,
     required: Boolean,
     additionalProperties: Map<String, Any>
 ) : Parameter(name, translationKey, order, type, validation, defaultValue, dependsOn, required, additionalProperties)
