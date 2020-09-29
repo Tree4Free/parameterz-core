2ae538e2352fa08a51979751d3026899b74ba527
Tree4Free <roli123@me.com>
2020-09-29 22:12:25 +0200

    [FIX] Revert translator service changes

--- src/main/kotlin/com/schuetz/parameterz/core/parameterdescriptors/translation/ParameterTranslatorService.kt
+++ src/main/kotlin/com/schuetz/parameterz/core/parameterdescriptors/translation/ParameterTranslatorService.kt
@@ -15,20 +15,25 @@ class ParameterTranslatorService<T : TranslationKey<T>>(
     fun translate(parameter: Parameter): Parameter {
         val lang = translationLanguageSource.getLanguage()
 
-        when {
-            lang == null -> return parameter.also { log.warn("No language provided. Skipping translation.") }
-            parameter.translationKey == null -> return parameter.also { log.warn("No translation key provided. Skipping translation.") }
+        if (lang == null) {
+            log.warn("No language provided. Skipping translation.")
+            return parameter
+        }
+
+        if (parameter.translationKey == null) {
+            log.warn("No translation key provided. Skipping translation.")
+            return parameter
         }
 
         return TranslatedParameter(
             order = parameter.order,
             name = parameter.name,
-            translationKey = parameter.translationKey!!,
+            translationKey = parameter.translationKey,
             defaultValue = parameter.defaultValue,
             dependsOn = parameter.dependsOn,
             validation = parameter.validation,
             required = parameter.required,
-            description = translationProvider.getTranslation(parameter.translationKey.description() as T, lang!!),
+            description = translationProvider.getTranslation(parameter.translationKey.description() as T, lang),
             displayName = translationProvider.getTranslation(parameter.translationKey.name() as T, lang),
             type = if (parameter.type is PredefinedParameterType) translateParameterType(parameter.translationKey, parameter.type, lang) else parameter.type,
             additionalProperties = parameter.additionalProperties
